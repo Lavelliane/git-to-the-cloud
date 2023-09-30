ef48f77b982e46032a8397701e0d98ad8db60c65
Vince Abella <vinceabella05@gmail.com>
2023-09-30 13:43:15 +0800

    [feature]added comments to the routes

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require("express");
+const express = require('express');
 
-const app = express()
+const app = express();
 
+//route going to root
 app.get('/', (req, res) => {
-    res.send("Hello world")
-})
+	res.send('Hello world');
+});
 
+//route going to /wazup
 app.get('/wazup', (req, res) => {
-    res.send("wazuppp")
-})
+	res.send('wazuppp');
+});
 
 app.listen(5000, () => {
-    console.log("listening on port 5000");
+	console.log('listening on port 5000');
 });
 
 // module.exports = server
